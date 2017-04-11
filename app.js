ca079a3a0298a98171e1e155bb889b1f77387b22
Kseniya <k.shychko@gmail.com>
2017-04-11 22:02:24 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+var swagger2aglio = require('swagger2aglio');
+
+var options = {
+    input: 'kshychko-ausdigital-dcp-1.0-swagger.yml',
+    themeVariables: 'flatly',
+    themeTemplate: 'triple'
+}
+
+swagger2aglio.convert(options, function (err, html) {
+    if (err) return console.log(err);
+    fs.writeFile("output.html", html)
+    console.log(html);
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
